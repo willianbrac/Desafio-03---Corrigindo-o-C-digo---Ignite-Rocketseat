a1b685b5531928b66292fea3897dfa4ecfa66aa0
willianbrac <66275588+willianbrac@users.noreply.github.com>
2021-07-28 21:37:09 -0500

    Challenge concluded

---
+++ src/index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0 
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find((repository) => {
+    return repository.id === id;
+  });
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  if(title){
+    repository.title = title;
+  }
+
+  if(url){
+    repository.url = url;
+  }
+
+  if(techs){
+    repository.techs = techs;
+  }
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex((repository) => {
+    return repository.id === id;
+  });
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex((repository) => {
+    return repository.id === id
+  });
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories[repositoryIndex].likes++;
+  const totalLikes = repositories[repositoryIndex].likes;
+  return response.json({ likes: totalLikes });
+});
+
+module.exports = app;
